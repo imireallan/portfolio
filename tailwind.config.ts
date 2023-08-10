f78c753dca35ea0b47a55f73574f6af9decb0816
Allan Imire <allanimire@gmail.com>
2023-08-10 20:06:20 +0300

    add fira font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "fira": "Fira Code, sans-serif",
+    },
   },
   plugins: [],
-} satisfies Config
-
+} satisfies Config;
